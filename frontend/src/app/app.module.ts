e8c5342fbe56ad5047491621c40e43dcf45430d2
Cub12 <seriy.cub@gmail.com>
2023-10-18 21:19:00 +0300

    fixed cass and added primeng

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,22 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagesComponent } from './pages/pages.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
 import { EntrantPageComponent } from './pages/entrant-page/entrant-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 import { StudentPageComponent } from './pages/student-page/student-page.component';
 import { SubdivisionsPageComponent } from './pages/subdivisions-page/subdivisions-page.component';
-import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent,
-    MainPageComponent,
+    ContactsPageComponent,
     EntrantPageComponent,
+    MainPageComponent,
     StudentPageComponent,
-    SubdivisionsPageComponent,
-    ContactsPageComponent
+    SubdivisionsPageComponent
   ],
   imports: [
     BrowserModule,
