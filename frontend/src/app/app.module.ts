69cd7fe72155166a09bd9039aab3c5e807e66f0c
Cub12 <seriy.cub@gmail.com>
2023-10-31 15:36:09 +0200

    add footer to all pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SubdivisionsPageComponent } from './pages/subdivisions-page/subdivisions-page.component';
 import { BaseHeaderModule } from "./pages/base-header/base-header.module";
+import {BaseFooterModule} from "./pages/base-footer/base-footer.module";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BaseHeaderModule } from "./pages/base-header/base-header.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BaseHeaderModule
+    BaseHeaderModule,
+      BaseFooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
