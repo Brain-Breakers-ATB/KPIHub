3bef1723b02b96544fbf1e0692275191e2958989
Bogdan <bogdanremuga@gmail.com>
2023-11-01 16:38:22 +0200

    added test backend api and sucsessful connect to frontend folder but without angular only to intex.html

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,67 @@
-import express, { Express, Request, Response } from "express";
+import express from 'express';
+import * as path from 'path';
+import * as http from 'http';
+import bodyParser from 'body-parser';
+import { BackendApi } from './routes/backendapi';
 
+class Server { 
+    public app: express.Application; 
+  
+    public static bootstrap(): Server { 
+        return new Server(); 
+    } 
+  
+    constructor() { 
+        // create expressjs application 
+        this.app = express(); 
 
-const app: Express = express();
-const port = process.env.PORT || 8000;
+        //configure application
+        this.config();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to KPIHub server');
-});
+        //configure routes
+        this.routes();
+    } 
 
-app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`);
-});
+    private config() {
+        //parsers for POST data
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: false}));
+
+        //static path to angular folder
+        this.app.use(express.static(path.join('../frontend', 'src')));
+
+        //Get port from environment and store in Express
+        const port = process.env.PORT || '3000';
+        this.app.set('port', port);
+
+        //Create HTTP server
+        const server = http.createServer(this.app);
+
+        //listen on provided port
+        server.listen(port, () => {
+            console.log(`Api running on localhost:${port}`);
+        });
+    }
+
+    private routes() {
+        //get router
+        let router: express.Router;
+        router = express.Router();
+
+        //creat routes
+        const api: BackendApi = new BackendApi();
+
+        //test API
+        router.get('/api/test', api.test.bind(api.test));
+
+        //use router middleware
+        this.app.use(router);
+
+        //catch all other routes and return the index file
+        this.app.get('*', (req: express.Request, res: express.Response) => {
+            res.sendFile(path.join('src/index.html'), { root: '../frontend' })
+        })
+    }
+}
+
+Server.bootstrap();
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SubdivisionsPageComponent } from './pages/subdivisions-page/subdivisions-page.component';
 import { BaseHeaderModule } from "./pages/base-header/base-header.module";
-import {BaseFooterModule} from "./pages/base-footer/base-footer.module";
+import { BaseFooterModule } from "./pages/base-footer/base-footer.module";
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import {BaseFooterModule} from "./pages/base-footer/base-footer.module";
     BrowserModule,
     AppRoutingModule,
     BaseHeaderModule,
-      BaseFooterModule
+    BaseFooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
