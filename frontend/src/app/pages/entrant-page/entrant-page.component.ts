285254054f55fd45fb1010c3d4483f6f660457ac
Cub12 <seriy.cub@gmail.com>
2023-11-04 01:36:09 +0200

    buttons for entrant page

--- frontend/src/app/pages/entrant-page/entrant-page.component.ts
+++ frontend/src/app/pages/entrant-page/entrant-page.component.ts
@@ -6,9 +6,31 @@ import { Component } from '@angular/core';
     styleUrls: ['./entrant-page.component.sass']
 })
 export class EntrantPageComponent {
-    activeBoxId: number | null = null; // Initialize with null
+    expandedBoxes: Set<number> = new Set<number>();
+
+    isBoxExpanded(boxId: number): boolean {
+        return this.expandedBoxes.has(boxId);
+    }
 
     expandBox(boxId: number) {
-        this.activeBoxId = boxId;
+        if (this.expandedBoxes.has(boxId)) {
+            this.expandedBoxes.delete(boxId);
+        } else {
+            this.expandedBoxes.add(boxId);
+        }
+
+        const content = document.getElementById(`content-${boxId}`);
+        if (content) {
+            const box = content.parentElement;
+            if (box) {
+                if (content.style.display === 'none' || content.style.display === '') {
+                    content.style.display = 'block';
+                    box.style.height = '400px';
+                } else {
+                    content.style.display = 'none';
+                    box.style.height = '113px';
+                }
+            }
+        }
     }
 }
