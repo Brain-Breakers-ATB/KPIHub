94322ac763f7c1a7f567219ac13ecb3deb770a31
LafRina <lafenkoirina5@gmail.com>
2023-11-03 13:50:35 +0200

    footer-and-header

--- frontend/src/app/pages/base-header/base-header.component.ts
+++ frontend/src/app/pages/base-header/base-header.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-base-header',
   templateUrl: './base-header.component.html',
   styleUrls: ['./base-header.component.sass']
 })
-export class BaseHeaderComponent{
+export class BaseHeaderComponent implements OnDestroy {
+  isDisplayed=true; 
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor(private router: Router) {
+    this.router.events.pipe(filter(event => event instanceof NavigationStart), takeUntil(this.destroy$)).subscribe((event) => {
+      this.isDisplayed= (event as NavigationStart).url === "/"
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
