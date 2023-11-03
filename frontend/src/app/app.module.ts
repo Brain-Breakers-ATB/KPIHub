ccf517113681c82714e3d7a4ba13577b3f5ce83f
Bogda <bogdanremuga@gmail.com>
2023-11-03 14:47:37 +0200

    pc commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContactsPageComponent } from './pages/contacts-page/contacts-page.compo
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SubdivisionsPageComponent } from './pages/subdivisions-page/subdivisions-page.component';
 import { BaseHeaderModule } from "./pages/base-header/base-header.module";
+import { BaseFooterComponent } from './pages/base-footer/base-footer.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { BaseHeaderModule } from "./pages/base-header/base-header.module";
     ContactsPageComponent,
     MainPageComponent,
     SubdivisionsPageComponent,
+    BaseFooterComponent
   ],
   imports: [
     BrowserModule,
