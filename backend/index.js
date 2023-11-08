eda39bf6d1f0756e8c90a8f55e31b11f04790dcc
Mast0 <bogdanremuga@gmail.com>
2023-11-08 14:47:18 +0200

    BB-24: Feedback form for contacts page

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ var Express = require("express");
 var Mongoclient = require("mongodb").MongoClient;
 var cors = require("cors");
 const multer = require("multer");
+const mongoose = require('mongoose')
 
 // Create an instance of the Express application
 var app = Express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use(Express.json())
 const port = 3000; // Define the port number for the server
 
 // MongoDB connection information
@@ -34,14 +36,16 @@ app.get('/api/:collection/GetItems', (request, response) => {
   });
 });
 
-// Route to add an item to a specific collection
-app.post('/api/:collection/AddItem', multer().none(), (request, response) => {
-  const collectionName = request.params.collection;
+// Route to add an feedback form to a feedback collection
+app.post('/api/feedback/AddItem', multer().none(), (request, response) => {
+  const collectionName = 'feedback';
   const collection = database.collection(collectionName);
 
   // Insert a document into the specified collection
   const dataToInsert = {
-    ...request.body, // Copy the data from the request body
+    name: request.body.name,
+    email: request.body.email,
+    message: request.body.message,
     createdAt: new Date(), // Add a "createdAt" timestamp
     createdBy: request.user, // Add information about the user who made the request
   };
