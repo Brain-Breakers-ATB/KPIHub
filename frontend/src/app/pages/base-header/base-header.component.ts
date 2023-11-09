02505a399064cf6264fedb4ff8361f01dbba9101
Cub12 <seriy.cub@gmail.com>
2023-11-09 15:22:53 +0200

    improved base-header code

--- frontend/src/app/pages/base-header/base-header.component.ts
+++ frontend/src/app/pages/base-header/base-header.component.ts
@@ -7,22 +7,33 @@ import { Subject, filter, takeUntil } from 'rxjs';
     templateUrl: './base-header.component.html',
     styleUrls: ['./base-header.component.sass']
 })
+
 export class BaseHeaderComponent implements OnDestroy {
-  isDisplayed=true;
   private destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(private router: Router) {
-    this.router.events.pipe(filter(event => event instanceof NavigationStart), takeUntil(this.destroy$)).subscribe((event) => {
-      this.isDisplayed= (event as NavigationStart).url === "/"
-    })
+    this.router.events.pipe(filter(event => event instanceof NavigationStart),
+        takeUntil(this.destroy$)).subscribe((event) => {
+            this.isDisplayed= (event as NavigationStart).url === "/"
+        })
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next(true);
+      this.destroy$.unsubscribe();
   }
 
   isMainPage() {
         return this.router.url === '/';
   }
 
+  isDisplayed: boolean = true;
 
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    this.destroy$.unsubscribe();
-  }
+  menu: { label: string, link: string }[] = [
+      { label: 'ВСТУПНИКУ', link: 'entrant' },
+      { label: 'СТУДЕНТУ', link: 'student' },
+      { label: 'ГАЛЕРЕЯ', link: 'gallery' },
+      { label: 'МАПА', link: 'map' },
+      { label: 'ІНСТИТУТИ & ФАКУЛЬТЕТИ', link: 'subdivisions' },
+      { label: 'КОНТАКТИ', link: 'contacts' }
+  ];
 }
