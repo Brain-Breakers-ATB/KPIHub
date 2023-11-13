0af99721d8e3e126257edb7bd5c34a5dd053ff2c
Xlebushek_lead <glebas9089@gmail.com>
2023-11-13 22:32:52 +0200

    Adaption Hlib

--- frontend/src/app/pages/base-header/base-header.module.ts
+++ frontend/src/app/pages/base-header/base-header.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { BaseHeaderRoutingModule } from './base-header-routing.module';
 import { RouterModule } from "@angular/router";
 import { BaseHeaderComponent } from "./base-header.component";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 
 @NgModule({
   declarations: [BaseHeaderComponent],
-  imports: [
-    CommonModule,
-    BaseHeaderRoutingModule,
-    RouterModule
-  ],
+    imports: [
+        CommonModule,
+        BaseHeaderRoutingModule,
+        RouterModule,
+        FlexLayoutModule
+    ],
   exports: [
     BaseHeaderComponent
   ]
