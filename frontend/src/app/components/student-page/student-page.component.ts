b632e38ebbc050c9556c38eef1578857b2709a0a
29lina <taranenko.alina2004@gmail.com>
2023-11-16 22:16:35 +0200

    Create services

--- frontend/src/app/components/student-page/student-page.component.ts
+++ frontend/src/app/components/student-page/student-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivitiesService } from 'src/app/services/activities.service';
+import { StudentTelegramChannelsService } from 'src/app/services/student-telegram-channels.service';
 import { Activity } from 'src/app/models/activities';
 import { take } from 'rxjs';
+import { StudentTgChannel } from 'src/app/models/studentTelegramChannels';
 
 @Component({
   selector: 'app-student-page',
@@ -13,19 +15,25 @@ import { take } from 'rxjs';
 export class StudentPageComponent implements OnInit {
     readonly APIUrl = "http://localhost:3000/api/";
 
-    //activities: Activity[] = [];
-    activities: any = [];
-    telegramChannels: any = [];
+    activities: Activity[] = [];
+    //activities: any = [];
+    telegramChannels: StudentTgChannel [] = [];
 
-    //constructor(private activitiesService: ActivitiesService) { }
-    constructor(private http: HttpClient) { }
-    /*ngOnInit() {
+    constructor(private activitiesService: ActivitiesService, private studentTgChannelsService: StudentTelegramChannelsService) { }
+
+
+    //constructor(private http: HttpClient) { }
+    ngOnInit() {
     this.activitiesService.getActivities().pipe(take(1)).subscribe((activities: Activity[]) => {
     this.activities = activities
     })
-    }*/
 
-    refreshItems () {
+    this.studentTgChannelsService.getStudentTgChannels().pipe(take(1)).subscribe((studentTgChannels: StudentTgChannel[]) => {
+    this.telegramChannels = studentTgChannels
+    })
+    }
+
+    /* refreshItems () {
         this.http.get(this.APIUrl+'activities/GetActivity').subscribe(data=>{
             this.activities=data;
         })
@@ -37,5 +45,5 @@ export class StudentPageComponent implements OnInit {
 
     ngOnInit() {
         this.refreshItems ();
-    }
+    } */
 }
