299b01966b6d6453a9e739537d44d95d15dc36b6
Serhiy Kondratenko <78702580+Cub12@users.noreply.github.com>
2023-11-23 20:00:12 +0200

    Update base-header-routing.module.ts

--- frontend/src/app/pages/base-header/base-header-routing.module.ts
+++ frontend/src/app/pages/base-header/base-header-routing.module.ts
@@ -4,12 +4,10 @@ import { MainPageComponent } from "../main-page/main-page.component";
 import { EntrantPageComponent } from "../entrant-page/entrant-page.component";
 import { StudentPageComponent } from "../student-page/student-page.component";
 import { ContactsPageComponent } from "../contacts-page/contacts-page.component";
-<<<<<<< Updated upstream:frontend/src/app/pages/base-header/base-header-routing.module.ts
+import { GalleryPageComponent } from "../gallery-page/gallery-page.component";
 import { SubdivisionsPageComponent } from "../subdivisions-page/subdivisions-page.component";
-=======
 import { MapPageComponent } from "../map-page/map-page.component";
 import {NotfoundComponent} from "../notfound/notfound.component";
->>>>>>> Stashed changes:frontend/src/app/components/base-header/base-header-routing.module.ts
 
 const routes: Routes = [
   { path: 'notfound', component: NotfoundComponent },
@@ -28,6 +26,11 @@ const routes: Routes = [
     loadChildren: () => import('../student-page/student-page.module').then((m) => m.StudentPageModule),
   },
   {
+      path: 'gallery',
+      component: GalleryPageComponent,
+      loadChildren: () => import('../gallery-page/gallery-page.module').then((m) => m.GalleryPageModule)
+  },  
+  {
     path: 'contacts',
     component: ContactsPageComponent,
       loadChildren: () => import('../contacts-page/contacts-page.module').then((m) => m.ContactsPageModule),
@@ -36,21 +39,17 @@ const routes: Routes = [
     path: 'subdivisions',
     component: SubdivisionsPageComponent,
   },
-<<<<<<< Updated upstream:frontend/src/app/pages/base-header/base-header-routing.module.ts
-=======
-
     {
         path: 'map',
         component: MapPageComponent,
         loadChildren: () => import('../map-page/map-page.module').then((m) => m.MapPageModule)
     },
     { path: '**', redirectTo: '/notfound' }
-
->>>>>>> Stashed changes:frontend/src/app/components/base-header/base-header-routing.module.ts
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+    
 export class BaseHeaderRoutingModule { }
