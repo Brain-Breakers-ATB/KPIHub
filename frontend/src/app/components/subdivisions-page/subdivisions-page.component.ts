6fc6d047822191da71c3d0c5fdbe03247093b2b2
Cub12 <seriy.cub@gmail.com>
2023-12-20 20:36:13 +0200

    fix for gallery and subdivisions

--- frontend/src/app/components/subdivisions-page/subdivisions-page.component.ts
+++ frontend/src/app/components/subdivisions-page/subdivisions-page.component.ts
@@ -19,7 +19,6 @@ import {Result} from "../../models/results";
 })
 
 export class SubdivisionsPageComponent implements OnInit, OnDestroy {
-    searchErrorMessage: string = ''; // Add this line
     isFilterActive: boolean = false;
     isDepartmentFilterActive: boolean = false;
     lastUniqueSearch: string = '';
@@ -36,7 +35,6 @@ export class SubdivisionsPageComponent implements OnInit, OnDestroy {
     private destroy$: Subject<boolean> = new Subject<boolean>();
     private departmentList!: SelectItemGroup[];
     showSearchResult: boolean = false;
-    instituteFilterSelected: boolean = false;
     public totalResults: number = 0;
 
     constructor(
@@ -119,16 +117,7 @@ export class SubdivisionsPageComponent implements OnInit, OnDestroy {
 
     onSearch() {
         const trimmedSearchInput = this.searchInput.trim();
-        const hasSelectedFilters = this.hasSelectedFilters();
-
-        if (trimmedSearchInput === '' && !hasSelectedFilters) {
-            // Set the error message to inform the user that the search field is empty
-            this.searchErrorMessage = 'Please enter a search query or select filters.';
-            return;
-        }
-        if (this.instituteFilterSelected && !this.hasSelectedFilters()) {
-            // Display a message when an institute filter is selected without selecting any department filter
-            this.searchErrorMessage = 'Please select at least one department along with the institute filter.';
+        if (trimmedSearchInput === '') {
             return;
 
         }
@@ -137,16 +126,7 @@ export class SubdivisionsPageComponent implements OnInit, OnDestroy {
         this.totalRecords = this.searchResults.length;
         this.addToSearchHistory(trimmedSearchInput);
 
-        // Check if the search is performed only with filters
-        const isSearchOnlyWithFilters = trimmedSearchInput === '' && hasSelectedFilters;
-
-        if (!isSearchOnlyWithFilters) {
-            // If not a search only with filters, add to search history
-            this.addToSearchHistory(trimmedSearchInput);
-        }
-
         this.showSearchHistory = false;
-
         const apiUrl = 'http://localhost:3000/api/search';
 
         // Modify the following lines to handle optional filters
@@ -212,14 +192,15 @@ export class SubdivisionsPageComponent implements OnInit, OnDestroy {
                     this.totalRecords = 0;
                 }
             );
-        this.searchErrorMessage = '';
         // Your existing search logic
+        this.addToSearchHistory(trimmedSearchInput);
         this.showSearchResult = true;
         this.totalRecords = this.searchResults.length;
+        this.addToSearchHistory(trimmedSearchInput);
+
         this.showSearchHistory = false;
 
     }
-
 
     onHistoryItemClick(historyItem: string) {
         // Перевіряємо, чи цей запит унікальний
@@ -300,11 +281,9 @@ export class SubdivisionsPageComponent implements OnInit, OnDestroy {
 
     onSelectChange(event: { value: Institute[] }) {
         const selectedInstitutes: string[] = event.value.map((instituteList: Institute) => instituteList.code);
-        this.filterDepartmentList = this.departmentList.filter((department: SelectItemGroup) => selectedInstitutes.
-        includes(department.label));
-
-        // Update the filter status
-        this.instituteFilterSelected = selectedInstitutes.length > 0 && this.filterDepartmentList.length === 0;
+        this.filterDepartmentList = this.departmentList.filter((department: SelectItemGroup) => selectedInstitutes.includes(department.label));
+        console.log(selectedInstitutes);
+        console.log(this.departmentList);
     }
 
     private getDepartments(): void {
@@ -342,19 +321,10 @@ export class SubdivisionsPageComponent implements OnInit, OnDestroy {
             .subscribe((institutes: Institute[]) => this.instituteList = institutes)
 
     }
-    private hasSelectedFilters(): boolean {
-        // Check if at least one filter is selected
-        return this.filterDepartmentList.some(group =>
-            group.items && group.items.length > 0 &&
-            group.items.some((item: SelectItem<any>) => (item as CustomSelectItem).checked !== undefined && (item as CustomSelectItem).checked)
-        ) || (this.selectedDepartments && this.selectedDepartments.length > 0);
-    }
+
 }
 
 interface CustomSelectItem {
     label: string;
     checked?: boolean; // Add this line if 'checked' is a valid property
-}
-interface CustomSelectItem extends SelectItem {
-    checked?: boolean;
 }
